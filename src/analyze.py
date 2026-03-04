59f1bb599f0db1cc5e2d7c2ac45c52ef9ace700b
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-03-04 12:05:41 -0300

    analsiis de la data para posterior muestraje de graficos

--- src/analyze.py
+++ src/analyze.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
+from pandas.core import groupby
+
 DATA_PROCESSED_PATH = Path(__file__).parent.parent / "data" / "processed"
 
 
@@ -16,7 +18,8 @@ def top_5_mas_vendidos_categoria(df):
             lambda x: x.sort_values(
                 "mount",
                 ascending=False,
-            ).head(5)
+            ).head(5),
+            include_groups=False,
         )
         .reset_index(drop=True)
     )
@@ -24,9 +27,93 @@ def top_5_mas_vendidos_categoria(df):
     return df_group
 
 
+def clientes_mas_revenue(df):
+    df = df.copy()
+
+    df_group = (
+        df.groupby(["client_id", "name_x"])["total"]
+        .sum()
+        .reset_index()
+        .sort_values(by="total", ascending=False)
+        .head(10)
+        .reset_index(drop=True)
+    )
+
+    return df_group
+
+
+def ticket_promedio_ciudad(df):
+    df = df.copy()
+
+    df = df[(df["country"] != "unknown") & (df["city"] != "unknown")]
+
+    df_group = df.groupby(["country", "city"])["total"].apply(
+        lambda x: x.mean().round(2)
+    )
+
+    return df_group
+
+
+def mes_pico_ventas(df):
+    df = df.copy()
+
+    df["order_date"] = pd.to_datetime(df["order_date"], format="mixed")
+
+    df_group = (
+        df.groupby(df["order_date"].dt.to_period("M"))["total"].sum().reset_index()
+    )
+
+    df_group["year"] = df_group["order_date"].dt.year
+
+    df_group = (
+        df_group.groupby("year")
+        .apply(lambda x: x.loc[x["total"].idxmax()], include_groups=False)
+        .reset_index(drop=True)
+    )
+
+    return df_group
+
+
+def clientes_sin_compras(df_master, df_clientes):
+    df = df_master.copy()
+
+    total_clientes = len(df_clientes)
+    clientes_compras = len(df.drop_duplicates(subset="client_id"))
+
+    clientes_sin_compras = total_clientes - clientes_compras
+    porcentaje = (clientes_sin_compras / total_clientes) * 100
+
+    return round(porcentaje, 2)
+
+
+def tasa_recompra_categoria(df_master):
+    df = df_master.copy()
+
+    df = df[df["category"] != "unknown"]
+    df_group = (
+        df.groupby(["category", "client_id"]).size().reset_index(name="num_compras")
+    )
+
+    def calcular_tasa(group):
+        total_clientes = len(group)
+        recurrentes = (group["num_compras"] > 1).sum()
+        return (recurrentes / total_clientes) * 100
+
+    tasa_recompra = (
+        df_group.groupby("category").apply(calcular_tasa, include_groups=False).round(2)
+    )
+
+    print(tasa_recompra)
+
+
 if __name__ == "__main__":
     df_master = pd.read_csv(DATA_PROCESSED_PATH / "master.csv")
-    print(df_master.head())
+    df_clientes = pd.read_csv(DATA_PROCESSED_PATH / "clients_clean.csv")
 
     top_5_por_categoria = top_5_mas_vendidos_categoria(df_master)
-    print(top_5_por_categoria)
+    clientes_mas_revenue = clientes_mas_revenue(df_master)
+    ticket_promedio_ciudad = ticket_promedio_ciudad(df_master)
+    mes_pico_ventas = mes_pico_ventas(df_master)
+    procentaje_clientes_sin_compras = clientes_sin_compras(df_master, df_clientes)
+
+    tasa_recompra_categoria(df_master)
